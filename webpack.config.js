c1aca4efabdf451bb6471028369f121b5dc99f7c
b3reketab <141371508+b3reketab@users.noreply.github.com>
2024-03-06 11:13:24 +0300

    modify   package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
@@ -12,7 +12,7 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     devServer: {
-        static: './dist'
+        static: './docs'
     },
     module: {
         rules: [
