29e6efdf76b007dd683d572e7beae73eba20ad13
b3reketab <141371508+b3reketab@users.noreply.github.com>
2024-03-06 11:00:42 +0300

    built page

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            title: 'todo app',
+            title: 'todo-list',
         })
     ]
 }
